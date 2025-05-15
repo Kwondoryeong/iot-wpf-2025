b2f3cf249df2eafea00eb1dbc66e4148f326392c
DHK <bazzi95@naver.com>
2025-05-15 09:21:48 +0900

    5일차 커밋

---
+++ day05/MovieFinder2025/MovieFinder2025/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MahApps.Metro.Controls.Dialogs;
+using MovieFinder2025.Helpers;
+using MovieFinder2025.ViewModels;
+using MovieFinder2025.Views;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MovieFinder2025
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Common.dialogCoordinator = DialogCoordinator.Instance; // .Instance; --> 싱글톤패턴 // == new DialogCoordinator(); 
+            var viewModel = new MoviesViewModel(Common.dialogCoordinator);
+            var view = new MoviesView
+            {
+                DataContext = viewModel,
+            };
+            view.Show();
+        }
+    }
+
+}
---
+++ day05/MovieFinder2025/MovieFinder2025/ViewModels/MoivesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MahApps.Metro.Controls.Dialogs;
+using MovieFinder2025.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFinder2025.ViewModels
+{
+    public partial class MoviesViewModel : ObservableObject
+    {
+        private readonly IDialogCoordinator dialogCoordinator;
+        public MoviesViewModel(IDialogCoordinator coordinator)
+        {
+            this.dialogCoordinator = coordinator;
+            Common.LOGGER.Info("MovieFinder2025 Start.");
+        }
+    }
+}
